dd8e9d0d98c95ac7e0cf65d8f73ff065011d9973
Jiwon <zest1923@gmail.com>
2022-04-20 23:01:14 +0900

    04/20 11724_연결 요소의 개수

--- Baekjoon/DFS_BFS/11724_connectedComponent.swift
+++ Baekjoon/DFS_BFS/11724_connectedComponent.swift
@@ -1,10 +1,26 @@
-func dfs(n: Int){
-    
+let datas = readLine()!.split(separator: " ").map{ Int($0)! }
+var graph = [[Int]](repeating: [], count: datas[0] + 1)
+var visited = [Bool](repeating: false, count: datas[0] + 1)
+var result = 0
+for _ in 0..<datas[1]{
+    let edges = readLine()!.split(separator: " ").map{ Int($0)! }
+    graph[edges[0]].append(edges[1])
+    graph[edges[1]].append(edges[0])
 }
+for node in 1...datas[0]{
+    if !visited[node]{
+        result += dfs(v: node)
+    }
+}
+print(result)
 
-var result: Int = 0
-let box: [Int] = readLine()!.split(separator: " ").map{Int($0)!}
-var graph = [[Int]]()
-for _ in 0..<box[1]{
-    graph.append( readLine()!.split(separator: " ").map{Int($0)!} )
+
+func dfs(v: Int) -> Int{
+    visited[v].toggle()
+    for node in graph[v]{
+        if !visited[node]{
+            dfs(v: node) // 여기서 호출된건 신경쓰지 않기!
+        }
+    }
+    return 1
 }
