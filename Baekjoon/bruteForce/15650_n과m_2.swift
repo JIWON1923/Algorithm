d85c58fb5860d0090464a0e7631052332fdd2dad
Jiwon <zest1923@gmail.com>
2022-04-29 23:17:13 +0900

    04/29 15650 n과 m(2) - update

--- Baekjoon/bruteForce/15650_n과m_2.swift
+++ Baekjoon/bruteForce/15650_n과m_2.swift
@@ -2,17 +2,17 @@ let input = readLine()!.split(separator: " ").map{ Int(String($0))! }
 let n = input[0], m = input[1]
 var result = [Int](repeating: 0, count: m)
 var resultP = ""
-rec_func(0)
+rec_func(0, 1)
 print(resultP)
 
-func rec_func(_ k: Int) {
+func rec_func(_ k: Int, _ j: Int) {
     if k == m {
         resultP += result.map{ String($0) }.joined(separator: " ") + "\n"
         return
     }
-    for i in 1 ... n {
-        if k != 0, result[k-1] >= i { continue }
+    if j > n { return }
+    for i in j ... n {
         result[k] = i
-        rec_func(k+1)
+        rec_func(k+1, i+1)
     }
 }
