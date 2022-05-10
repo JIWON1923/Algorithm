8fe8e6c452c4253d99f6ccd2e242698e1eb8c650
Jiwon <zest1923@gmail.com>
2022-05-10 22:53:08 +0900

    05/10 1182_부분수열의 합-시간 줄이기

--- Baekjoon/bruteForce/1182_sumOfSubsequence.swift
+++ Baekjoon/bruteForce/1182_sumOfSubsequence.swift
@@ -1,21 +1,22 @@
 let input = readLine()!.split(separator: " ").map{ Int($0)! }
+let (n, s) = (input[0], input[1])
 let num = readLine()!.split(separator: " ").map{ Int($0)! }
 var visited = [Bool](repeating: false, count: input[0])
 var result = 0
 
-recFunc(k: 0, sum: 0)
-if input[1] == 0 { result -= 1}
+recFunc(0, 0)
+if s == 0 { result -= 1 }
 print(result)
 
-func recFunc(k: Int, sum: Int) {
-    if sum == input[1] {
+func recFunc(_ k: Int, _ sum: Int) {
+    if sum == s {
         result += 1
     }
     
-    for i in k ..< input[0] {
+    for i in k ..< n {
         if visited[i] { continue }
         visited[i] = true
-        recFunc(k: i + 1, sum: sum + num[i])
+        recFunc(i + 1, sum + num[i])
         visited[i] = false
     }
 }
