06329900de72a203069eb7dd5224455cf00a1586
Jiwon <zest1923@gmail.com>
2022-05-11 22:02:14 +0900

    05/11 1759_암호 만들기  - 시간 줄임
    1. contains연산 대신 switch문 사용
    2. index를 전역변수로 사용

--- Baekjoon/bruteForce/1759_createPassword.swift
+++ Baekjoon/bruteForce/1759_createPassword.swift
@@ -1,18 +1,15 @@
-//vowels-> 모음 / consonants-> 자음
-
-import Foundation
 let input = readLine()!.split(separator: " ").map{ Int($0)! }
 let (l, c) = (input[0], input[1])
-var char = readLine()!.split(separator: " ").map{ String($0) }
+var char = readLine()!.split(separator: " ").map{ String($0) }.sorted()
 var visited = [Bool](repeating: false, count: c)
 var result = [String](repeating: "", count: l)
 var results = ""
-char.sort()
+var index = 0
 
-recFunc(0, 0, 0, 0)
+recFunc(0, 0, 0)
 print(results)
 
-func recFunc(_ k: Int, _ vowels: Int, _ consonants: Int, _ index: Int) {
+func recFunc(_ k: Int, _ vowels: Int, _ consonants: Int) {
     if k == l {
         if vowels >= 1, consonants >= 2 {
             results += result.map{ String($0) }.joined(separator: "") + "\n"
@@ -23,11 +20,14 @@ func recFunc(_ k: Int, _ vowels: Int, _ consonants: Int, _ index: Int) {
         if visited[i] { continue }
         visited[i] = true
         result[k] = char[i]
-        if "aeiou".contains( char[i] ) {
-            recFunc(k+1, vowels+1, consonants, i)
-        } else {
-            recFunc(k+1, vowels, consonants+1, i)
+        index = i
+        switch char[i] {
+        case "a", "e", "i", "o", "u":
+            recFunc(k+1, vowels+1, consonants)
+        default:
+            recFunc(k+1, vowels, consonants+1)
         }
+        
         visited[i] = false
     }
 }
