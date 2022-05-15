bc3fbe3c27ff6700d4099334f947795dbbbe144e
Jiwon <zest1923@gmail.com>
2022-05-15 10:25:09 +0900

    05/15 Dictionary 사용, 시간 단축

--- Baekjoon/sort/20291_파일정리.swift
+++ Baekjoon/sort/20291_파일정리.swift
@@ -3,10 +3,10 @@ var fileSuffix = [String: Int]()
 for _ in 0 ..< n {
     let file = readLine()!.split(separator: ".").map{ String($0) }
     let (_, suffix) = (file[0], file[1])
-    if fileSuffix.keys.contains(suffix) { fileSuffix[suffix]! += 1 }
-    else { fileSuffix[suffix] = 1}
+    if fileSuffix[suffix] != nil { fileSuffix[suffix]! += 1 }
+    else { fileSuffix[suffix] = 1 }
 }
-let result = fileSuffix.keys.sorted()
-for i in result {
-    print(i, fileSuffix[i]!)
+let result = fileSuffix.sorted{ $0.0 < $1.0 }
+for (key, value) in result {
+    print(key, value)
 }
