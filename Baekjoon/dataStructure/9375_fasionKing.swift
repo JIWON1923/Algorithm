70ea63215fe9b86b1eb17a03ac9c56bf6302cd11
Jiwon <zest1923@gmail.com>
2022-06-03 23:28:05 +0900

    06/03 패션왕 신혜빈 코드 수정
    코드 작성 규칙

--- Baekjoon/dataStructure/9375_fasionKing.swift
+++ Baekjoon/dataStructure/9375_fasionKing.swift
@@ -1,31 +1,30 @@
-for _ in 0..<Int(readLine()!)!{
+for _ in 0..<Int(readLine()!)! {
     var result_arr: [Int] = []
     var dic = [String: [String]]()
     let n = Int(readLine()!)!
-    if n == 0{
+    if n == 0 {
         print(0)
         continue
     }
-    for _ in 0..<n{ // n만큼 반복
+    for _ in 0..<n { // n만큼 반복
         let value: [String] = readLine()!.split(separator: " ").map{ String($0) }
         
-        if dic.keys.contains(value[1]){
+        if dic.keys.contains(value[1]) {
             dic[value[1]]!.append(value[0])
         } else {
             dic[value[1]] = [value[0]]
         }
     }
-    for (_, value) in dic{
+    for (_, value) in dic {
         result_arr.append(value.count + 1)
     }
     print(mul(numbers: result_arr) - 1)
-    
 }
 
-func sum(numbers: [Int]) -> Int{
+func sum(numbers: [Int]) -> Int {
     numbers.reduce(0, +)
 }
 
-func mul(numbers: [Int]) -> Int{
+func mul(numbers: [Int]) -> Int {
     numbers.reduce(1, *)
 }
