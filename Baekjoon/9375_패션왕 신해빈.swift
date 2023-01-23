3ac1a889c99a65e60b193a4711ee0824019131a9
JIWON1923 <zest1923@gmail.com>
2023-01-24 02:36:02 +0900

    01/24 [Refactor] 패션왕 신해빈

--- Baekjoon/9375_패션왕 신해빈.swift
+++ Baekjoon/9375_패션왕 신해빈.swift
@@ -1,30 +1,28 @@
-//1. 테스트 케이스를 입력받아 반복한다.
-let input = Int(readLine()!)!
-for _ in 0..<input {
-    print(combination())
-}
+var input = Int(readLine()!)!
 
-func combination() -> Int {
-    var dict = [String: [String]]()
+//1. 테스트 케이스를 입력받아 반복한다.
+for _ in 0..<input {
+    var dict: [String: Int] = [:]
+    
     
     //2. n을 입력받는다.
-    let n = Int(readLine()!)!
-    
-    //3. 이름과 종류를 입력받고, 종류를 key, 이름을 value로 딕셔너리에 저장한다.
-    for _ in 0..<n {
-        let input = readLine()!.split(separator: " ").map { String($0) }
-        let value = input[0], key = input[1]
+    input = Int(readLine()!)!
+    for _ in 0..<input {
+        
+        //3. 이름과 종류를 입력받고, 종류를 key에 저장한다. 이름은 중복될 수 없으므로, 횟수를 저장한다.
+        let data = readLine()!.split(separator: " ").map { String($0) }
+        let key = data[1]
         if dict.keys.contains(key) {
-            dict[key]!.append(value)
+            dict[key]! += 1
         } else {
-            dict[key] = [value]
+            dict[key] = 1
         }
     }
     
     //4. 딕셔너리의 종류들 간 조합의 수를 계산하고 1을 빼서 출력한다 (알몸인 경우 제외)
-    let values = dict.values.map { $0.count + 1 }
-    return values.reduce(1, *) - 1
-    
+    let result = dict.values.map { $0 + 1 }.reduce(1, *) - 1
+    print(result)
+}
     /*
      3 -> 1 : 3가지
      hat headgear
@@ -35,4 +33,3 @@ func combination() -> Int {
      sunglasses = [eyewear] -> 2
      -> 6 - 1 = 5
      */
-}
