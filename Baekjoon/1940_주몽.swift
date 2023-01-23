bc7c9229c561f47f4ae37474f41a6576f9069875
JIWON1923 <zest1923@gmail.com>
2023-01-24 03:04:37 +0900

    01/24 [Refactor] 주몽

--- Baekjoon/1940_주몽.swift
+++ Baekjoon/1940_주몽.swift
@@ -7,19 +7,24 @@ let m = Int(readLine()!)!
 //3. 재료들을 입력받는다.
 var materials = readLine()!.split(separator: " ").map { Int($0) }
 
-//4. 모든 조합을 구하며, 합이 m 일 때에만 조합으로 취급한다.
-var result = 0
-for i in 0..<n {
-    for j in i+1..<n {
-        guard let first = materials[i],
-              let second = materials[j] else { continue }
-        if first + second == m {
-            result += 1
-            materials[i] = nil
-            materials[j] = nil
+//4. m이 20만이 넘는다면, 유효한 갑옷을 만들 수 없다.
+if m > 200000 {
+    print(0)
+} else {
+    //4. 모든 조합을 구하며, 합이 m 일 때에만 조합으로 취급한다.
+    var result = 0
+    for i in 0..<n {
+        for j in i+1..<n {
+            guard let first = materials[i],
+                  let second = materials[j] else { continue }
+            if first + second == m {
+                result += 1
+                materials[i] = nil
+                materials[j] = nil
+            }
         }
     }
+    
+    //5. 나온 결과의 수를 계산하여 출력한다.
+    print(result)
 }
-
-//5. 나온 결과의 수를 계산하여 출력한다.
-print(result)
