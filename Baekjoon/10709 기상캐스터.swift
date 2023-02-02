c48bf706f0faed6ab9f5daba2686af62641e9202
JIWON1923 <zest1923@gmail.com>
2023-02-02 19:13:03 +0900

    02/02 [Refactor] 기상캐스터

--- Baekjoon/10709 기상캐스터.swift
+++ Baekjoon/10709 기상캐스터.swift
@@ -1,26 +1,23 @@
 import Foundation
 
-//1. h, w을 입력받는다.
+//1. h, m을 입력받는다.
 let input = readLine()!.split(separator: " ").map { Int($0)! }
 let (h, w) = (input[0], input[1])
 
 //2. map를 선언한다. (최종 결과)
-var map = [[Int]](repeating: [Int](repeating: 0, count: w), count: h)
+var map = [[Int]](repeating:[Int](repeating: -1, count: w), count: h)
+var result = ""
 
 //3. 구름을 입력받고, vistied에 적용한다.
 for y in 0..<h {
-    let info = readLine()!.map { $0 }
-    for x in 0..<w {
-        guard info[x] == "c" else { continue }
-        for i in x..<w {
-            map[y][i] = i-x
+    let clouds = readLine()!.map { $0 }
+    for i in 0..<w {
+        guard clouds[i] == "c" else { continue }
+        for x in i..<w {
+            map[y][x] = x - i
         }
-        map[y][x] = -2
     }
-    map[y] = map[y].map { $0 == 0 ? -1 : $0 }
-    map[y] = map[y].map { $0 == -2 ? 0 : $0 }
+    result += map[y].map { String($0) }.joined(separator: " ") + "\n"
 }
+print(result)
 
-for y in map {
-    print(y.map { String($0) }.joined(separator: " "))
-}
