52c6865472e0cd423afe96fbe6988fbc52d3a511
JIWON1923 <zest1923@gmail.com>
2023-02-23 15:28:24 +0900

    02/23 오큰수

---
+++ Baekjoon/17298 오큰수.swift
@@ -0,0 +1,26 @@
+//1. 수열을 입력받는다
+//2. 오큰수를 계산한다
+/*
+ 값을 입력받는다.
+ 입력받은 값과 직전 값을 비교한다. 만약 입력받은 값이 크다면, 해당 값은 직전 값의 오큰수이다. (stack 젤 위에 있는 것이 더 작을때까지 반복한다.)
+ 
+ */
+
+let n = Int(readLine()!)!
+let numbers = readLine()!.split { $0 == " " }.map { Int($0)! }
+var stack = [Int]()
+var result = [Int](repeating: -1, count: n)
+
+for i in 0 ..< n {
+    guard !stack.isEmpty else {
+        stack.append(i)
+        continue
+    }
+    for _ in 0 ..< stack.count {
+        guard numbers[stack.last!] < numbers[i] else { break }
+        let prev = stack.removeLast()
+        result[prev] = numbers[i]
+    }
+    stack.append(i)
+}
+result.forEach { print($0, terminator: " ") }
