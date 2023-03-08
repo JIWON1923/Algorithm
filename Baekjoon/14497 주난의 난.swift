fcdf4c1af492d575c53bd874c6f0326684afa4b0
JIWON1923 <zest1923@gmail.com>
2023-03-08 21:14:58 +0900

    03/08 주난의 난 - 시간초과

--- Baekjoon/14497 주난의 난.swift
+++ Baekjoon/14497 주난의 난.swift
@@ -6,8 +6,8 @@ input = readLine()!.split { $0 == " " }.map { Int($0)! - 1 }
 let (startY, startX, goalY, goalX) = (input[0], input[1], input[2], input[3])
 var visited = [[Int]](repeating: [Int](repeating: 0, count: n), count: m)
 var map = [[Character]]()
-var tempPositions = [(Int, Int)]()
-var bfsQueue = [(startY, startX)]
+var tempPositions = [Int]()
+var bfsQueue = [ 1000 * startY + startX ]
 var count = 0
 
 for _ in 0 ..< m {
@@ -18,7 +18,8 @@ while map[goalY][goalX] == "#" {
     count += 1
     
     while !bfsQueue.isEmpty {
-        let (y, x) = bfsQueue.removeFirst()
+        let num = bfsQueue.removeFirst()
+        let (y, x) = (num / 1000, num % 1000)
         for i in 0 ..< 4 {
             let ny = dy[i] + y
             let nx = dx[i] + x
@@ -27,9 +28,9 @@ while map[goalY][goalX] == "#" {
             visited[ny][nx] = count
             if map[ny][nx] != "0" {
                 map[ny][nx] = "0"
-                tempPositions.append((ny, nx))
+                tempPositions.append(1000 * ny + nx)
             } else {
-                bfsQueue.append((ny, nx))
+                bfsQueue.append(1000 * ny + nx)
             }
         }
     }
